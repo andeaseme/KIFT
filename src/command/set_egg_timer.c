031e861b940489d00bda2dac432d768b764061ef
robert passafaro <rpassafa@e1z2r7p39.42.us.org>
2017-06-13 20:03:21 -0700

    pushing egg timer fix

--- src/command/set_egg_timer.c
+++ src/command/set_egg_timer.c
@@ -6,7 +6,7 @@
 /*   By: aphan <aphan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/08 14:50:22 by aphan             #+#    #+#             */
-/*   Updated: 2017/06/12 22:31:09 by rpassafa         ###   ########.fr       */
+/*   Updated: 2017/06/13 20:02:57 by rpassafa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,17 @@
 
 int			cmd_seteggtimer(struct s_con *temp)
 {
-	char const	*targets[] = {"hard", "soft", NULL};
-	char		*speech;
+	// char const	*targets[] = {"hard", "soft", NULL};
+	// char		*speech;
 	clock_t		time;
 	clock_t		start;
 
 	start = clock();
-	printf("Setting egg timer...\n");
-	system("./SAM/sam 'Setting egg timer. hard or soft boil.'");
-	send_voice(temp);
-	speech = receive_string(temp);
-	if (find_string(speech, targets))
-	{
-		time = 24000;
-		printf("four minute timer set\n");
-		system("./SAM/sam 'four minute timer set'");
-	}
-	else
-	{
-		time = 5000000;
-		printf("eight minute timer set\n");
-		system("./SAM/sam 'Eight minute timer set'");
-	}
+	time = 5000000;
+	if (temp)
+		;
+	printf("eight minute timer set\n");
+	system("./SAM/sam 'Eight minute timer set'");
 	while (time > clock() - start)
 	;
 	printf("your egg is ready!\n");
