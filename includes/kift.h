3dc56ded119212e819ea954f8030fc82291532f3
Ryley Matos <rmatos@e1z2r7p44.42.us.org>
2017-06-14 16:44:26 -0700

    actual voice recognition

--- includes/kift.h
+++ includes/kift.h
@@ -6,7 +6,7 @@
 /*   By: aphan <aphan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/07 16:00:37 by aphan             #+#    #+#             */
-/*   Updated: 2017/06/13 21:04:00 by rmatos           ###   ########.fr       */
+/*   Updated: 2017/06/14 16:38:53 by rmatos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@
 #define MAP 		"~/.brew/Cellar/cmu-sphinxtrain/HEAD-eb8bfba/libexec/sphinxtrain/map_adapt"
 
 #define HMMDIR      "ROB"
-#define LANGDIR     "ROB/en-us.lm.bin"
-#define DICTDIR     "ROB/cmudict-en-us.dict"
+#define LANGDIR     "2823.lm"
+#define DICTDIR     "2823.dic"
 #define READ_SIZE   2048
 
 # define RESET write(1, "\x1b[0m", 4)
