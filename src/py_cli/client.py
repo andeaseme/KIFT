7c08cdbdaa545031fc3057f5d047af7d1dbfd049
robert passafaro <rpassafa@e1z2r5p13.42.us.org>
2017-06-15 20:00:28 -0700

    python client works now

--- src/py_cli/client.py
+++ src/py_cli/client.py
@@ -2,31 +2,35 @@
 import socket               # Import socket module
 import os
 from bitarray import bitarray
+from ctypes import *
 
+HOST = "127.0.0.1"
+PORT = 22005
 
+HOST = raw_input("Enter IP Default (127.0.0.1) if left empty")
+if HOST is "":
+	HOST = "127.0.0.1"
+ans = raw_input("Enter PORT Default (22005) if left empty")
+if ans is "":
+	PORT = 22005
+else:
+	PORT = int(ans)
 
-HOST = "127.0.0.1"		  # The remote host
-PORT = 22005# The same port as used by the server
+print "HOST:" + HOST
+print "PORT:" + str(PORT)
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server_address = ("127.0.0.1, 10000)
-# sock.connect(server_address)
-# # call(["rec", "-c 1 -r 16000 -b 16 recording.wav gain +5 silence 1 0.1 3% 1 2.0 3%"])
 os.system("rec -c 1 -r 16000 -b 16 recording.wav gain +5 silence 1 0.1 3% 1 2.0 3%")
 i = long(os.path.getsize("recording.wav"));
 print i;
-# print str(bin(i))
-s.send(i)
-# s.send("recording.wav")
-# with open('recording.wav', 'rb') as f:
-#   for l in f: s.sendall(l)
+test = c_long(i)
+s.send(bytearray(test))
 f =  open('recording.wav', 'rb')
 temp = f.read(i)
 s.send(temp)
 size = s.recv(4)
-size = int(size)
-print size
-ret = s.recv(size)
-# print s.recv(1024)
+buf = ''
+data = s.recv(1024)
+print data
 s.close
