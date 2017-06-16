5faa2fcf77b5b3cc505c24460a1803a7d606b7e1
robert passafaro <rpassafa@e1z2r5p13.42.us.org>
2017-06-15 17:02:52 -0700

    python client

--- src/py_cli/client.py
+++ src/py_cli/client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python           # This is client.py file
 import socket               # Import socket module
-from subprocess import call
+import os
+
 
 
 # def functionname( parameters ):
@@ -12,7 +13,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 22005
 print ("HERE")
 s.connect(('127.0.0.1', port))
-call(["rec", "-c 1 -r 16000 -b 16 recording.wav gain +5 silence 1 0.1 3% 1 2.0 3%"])
+# call(["rec", "-c 1 -r 16000 -b 16 recording.wav gain +5 silence 1 0.1 3% 1 2.0 3%"])
+os.system("rec -c 1 -r 16000 -b 16 recording.wav gain +5 silence 1 0.1 3% 1 2.0 3%")
+
+i = long(os.path.getsize("recording.wav"));
+print i;
 
 # print s.recv(1024)
 s.close
