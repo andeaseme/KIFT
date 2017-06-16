ee246ed1387c5b62d6874f57d4f1b554a525c02d
robert passafaro <rpassafa@e1z2r5p13.42.us.org>
2017-06-15 19:18:34 -0700

    python client

--- src/py_cli/client.py
+++ src/py_cli/client.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python           # This is client.py file
 import socket               # Import socket module
 import os
+from bitarray import bitarray
 
 
 
-# def functionname( parameters ):
-
-
-
+HOST = "127.0.0.1"		  # The remote host
+PORT = 22005# The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#host = socket.gethostname()
-port = 22005
-print ("HERE")
-s.connect(('127.0.0.1', port))
-# call(["rec", "-c 1 -r 16000 -b 16 recording.wav gain +5 silence 1 0.1 3% 1 2.0 3%"])
+s.connect((HOST, PORT))
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server_address = ("127.0.0.1, 10000)
+# sock.connect(server_address)
+# # call(["rec", "-c 1 -r 16000 -b 16 recording.wav gain +5 silence 1 0.1 3% 1 2.0 3%"])
 os.system("rec -c 1 -r 16000 -b 16 recording.wav gain +5 silence 1 0.1 3% 1 2.0 3%")
-
 i = long(os.path.getsize("recording.wav"));
 print i;
-
+# print str(bin(i))
+s.send(i)
+# s.send("recording.wav")
+# with open('recording.wav', 'rb') as f:
+#   for l in f: s.sendall(l)
+f =  open('recording.wav', 'rb')
+temp = f.read(i)
+s.send(temp)
+size = s.recv(4)
+size = int(size)
+print size
+ret = s.recv(size)
 # print s.recv(1024)
 s.close
